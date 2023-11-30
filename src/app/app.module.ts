4d524a7baf88f10ceab73731def1224baa021ced
João Paulo <joaowais@gmail.com>
2023-11-30 16:01:31 -0300

    Topper Adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { TopperComponent } from './shared/topper/topper.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AboutComponent } from './pages/about/about.component';
     FooterComponent,
     HomeComponent,
     AboutComponent,
+    TopperComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/shared/topper/topper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topper',
+  templateUrl: './topper.component.html',
+  styleUrls: ['./topper.component.css']
+})
+export class TopperComponent {
+
+}
